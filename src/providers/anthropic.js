a988c219c487958fa860b4794fa7720e02321a2a
Louis Knight-Webb <louis@bloop.ai>
2023-11-10 16:19:02 -0800

    Fix Claude 2 and Claude instant support

--- src/providers/anthropic.js
+++ src/providers/anthropic.js
@@ -1,15 +1,32 @@
+import Anthropic from '@anthropic-ai/sdk';
 import { countTokens } from '@anthropic-ai/tokenizer';
 
 import BaseProvider from './base.js';
 
 export default class AnthropicProvider extends BaseProvider {
   static getModels() {
-    return [{ name: 'claude-2', maxTokens: 100000 }];
+    return [
+      { name: 'claude-2.0', maxTokens: 100000 },
+      { name: 'claude-instant-1.2', maxTokens: 100000 },
+    ];
   }
 
   async countTokens(text) {
     return countTokens(text);
   }
 
-  generateCompletion(str) {}
+  async generateCompletion(text) {
+    const anthropicConfig = new Anthropic({
+      apiKey: process.env.ANTHROPIC_KEY,
+    });
+
+    const completion = await anthropicConfig.completions.create({
+      model: this.modelName,
+      max_tokens_to_sample: 300,
+      temperature: 0,
+      prompt: `${Anthropic.HUMAN_PROMPT} ${text} ${Anthropic.AI_PROMPT}`,
+    });
+
+    return completion.completion;
+  }
 }
