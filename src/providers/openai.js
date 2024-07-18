cec22976b42606dc21a60143f6577e91ef506714
Louis Knight-Webb <louis@bloop.ai>
2024-07-18 18:58:35 +0100

    Add new tokeniser for 4o models and add 4o mini

--- src/providers/openai.js
+++ src/providers/openai.js
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
-import { encode } from 'gpt-tokenizer';
+import { encode as encodeDefault } from 'gpt-tokenizer';
+import { encode as encode4oFamily } from 'gpt-tokenizer/model/gpt-4o';
 
 import BaseProvider from './base.js';
 
@@ -11,14 +12,21 @@ export default class OpenAIProvider extends BaseProvider {
       { name: 'gpt-4-32k-0613', maxTokens: 32000 },
       { name: 'gpt-4-1106-preview', maxTokens: 128000 },
       { name: 'gpt-4o', maxTokens: 128000 },
+      { name: 'gpt-4o-mini', maxTokens: 128000 },
       { name: 'gpt-3.5-turbo-1106', maxTokens: 16000 },
       { name: 'gpt-3.5-turbo-16k-0613', maxTokens: 16000 },
+      { name: 'gpt-3.5-turbo-0125', maxTokens: 16000 },
     ];
   }
 
   async countTokens(text) {
-    const tokens = encode(text);
-    return tokens.length;
+    if (['gpt-4o', 'gpt-4o-mini'].includes(this.modelName)) {
+      const tokens = encode4oFamily(text);
+      return tokens.length;
+    } else {
+      const tokens = encodeDefault(text);
+      return tokens.length;
+    }
   }
 
   async generateCompletion(corpus, prompt) {
